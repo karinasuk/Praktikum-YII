49141e0e8cd67222c03071af41a05d3fb26bd38f
ichal.faiez17 <ichal.faiez17@gmail.com>
2019-03-25 13:21:21 +0700

    second push

---
+++ frontend/views/mahasiswa/index.php
@@ -0,0 +1,52 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>Daftar Mahasiswa</h1>
+
+<div class="container-fluid">
+	<div class ="row">
+		<table class="table table-hover table-sm table-bordered">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nama Mahasiswa</th>
+					<th>Program Studi</th>
+					<th>Semester</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td> <?=$sukma?> </td>
+					<td> <?=$si?> </td>
+					<td> <?=$smt4?> </td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td> <?=$farras?> </td>
+					<td> <?=$si?> </td>
+					<td> <?=$smt4?> </td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td> <?=$jennie?> </td>
+					<td> <?=$si?> </td>
+					<td> <?=$smt4?> </td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td> <?=$yayah?> </td>
+					<td> <?=$si?> </td>
+					<td> <?=$smt4?> </td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td> <?=$aceng?> </td>
+					<td> <?=$ti?> </td>
+					<td> <?=$smt1?> </td>
+				</tr>
+				
+			</tbody>
+		</table>
+	</div>
+</div>
